415850b99eebc5a8062d9b4c1a08fa65d7d82dd6
springbak <bmpark0403@naver.com>
2022-10-03 00:41:44 +0900

    feat(InputValidation): 재게임 의사에 대한 input 값을 검증하는 기능 추가
    validateRegameInputFromUser - 재게임 의사에 대한 input 값을 검증하는 기능 추가

--- src/main/java/baseball/InputValidation.java
+++ src/main/java/baseball/InputValidation.java
@@ -1,5 +1,16 @@
 package baseball;
-
+/**
+ * packageName    : baseball
+ * fileName       : InputValidation
+ * author         : BOM
+ * date           : 2022-09-30
+ * description    : 사용자의 입력값을 검증하는 class
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-09-30        BOM       최초 생성
+ * 2022-10-02        BOM       validateRegameInputFromUser 추가
+ */
 public class InputValidation {
     private int gameNumberLength;
 
@@ -7,6 +18,7 @@ public class InputValidation {
         this.gameNumberLength = length;
     }
 
+    /*사용자가 추측값으로 입력한 input을 검증하는 메서드*/
     public void validateInputNumberFromUser(String input) {
 
         if (input.equals(""))
@@ -18,6 +30,15 @@ public class InputValidation {
         }
         if (input.length() != gameNumberLength)
             throw new IllegalArgumentException("입력값의 자리수가 너무 적거나 많습니다. " + gameNumberLength + "자리 수를 입력해주세요!!!!");
+
+    }
+    /*사용자가 재게임 의사로 입력한 input을 검증하는 메서드*/
+    public void validateRegameInputFromUser(String input) {
+
+        if (input.equals(""))
+            throw new IllegalArgumentException("입력값이 없습니다. 1이나 2를 입력해주세요!!!!");
+        if (!input.equals("1") && !input.equals("2"))
+            throw new IllegalArgumentException("입력값이 1이나 2가 아닙니다. 1이나 2를 입력해주세요!!!!");
 
     }
 }
