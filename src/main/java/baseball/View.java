bc308545a36a2dc0d2451b004f2227ec2c13e144
springbak <bmpark0403@naver.com>
2022-10-02 22:47:46 +0900

    feat(View): 사용자 입력값에 대한 힌트를 출력하는 기능 추가
    printResultOfGuess 메서드 추가 - 사용자 입력값에 대한 힌트 출력
    printGameInterface 메서드 추가
    - 게임 인터페이스를 출력하는 메서드

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,4 +1,35 @@
 package baseball;
 
+/**
+ * packageName    : baseball
+ * fileName       : View
+ * author         : BOM
+ * date           : 2022-09-30
+ * description    : 사용자 interface 기능을 하는 class
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-09-30        BOM       최초 생성
+ */
 public class View {
+
+    public void printGameInterface(){
+        System.out.print("숫자를입력해주세요:");
+    }
+
+    /*사용자 추측값에 대한 힌트를 출력하는 기능*/
+    public void printResultOfGuess(int ballCount, int strikeCount) {
+        String resultSentence = "";
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        if (ballCount > 0)
+            resultSentence += ballCount + "볼 ";
+        if (strikeCount > 0)
+            resultSentence += strikeCount + "스트라이크";
+
+        System.out.println(resultSentence);
+    }
 }
