365d5662d32fdeaff59f6fc1b44801d6e53177f2
springbak <bmpark0403@naver.com>
2022-10-03 00:36:11 +0900

    feat(Application): 게임을 진행하는 controller 부분 기능 작성
    runGame - 게임을 진행시키는 메서드
    getUserGuess - 사용자에게 추측값을 받아오는 메서드
    askNewGame -
    재게임 의사를 묻는 메서드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,51 @@
+
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
+    public static final int gameNumberLength = 3;
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameNumber gameNumber = new GameNumber(gameNumberLength);
+        InputValidation inputValidation = new InputValidation(gameNumberLength);
+        View view = new View();
+        Hint hint = new Hint();
+        String gameStatus = "1";
+        while (gameStatus.equals("1")) {
+            runGame(hint, view, inputValidation, gameNumber);
+            gameStatus = askNewGame(view, inputValidation);
+        }
+    }
+
+    /*게임을 진행시키는 메서드*/
+    public static void runGame(Hint hint, View view, InputValidation inputValidation, GameNumber gameNumber) {
+        hint.resetCounts();
+        String gameAnswerNumber = gameNumber.getRandomNumberString();
+        String input;
+        while (hint.getStrike() < gameNumberLength) {
+            hint.resetCounts();
+            input = getUserGuess(view,inputValidation);
+            gameNumber.getHintFromUserInput(input, gameAnswerNumber, hint);
+            view.printResultOfGuess(hint.getBall(), hint.getStrike());
+        }
+    }
+    /*사용자에게 추측값을 받아오는 메서드*/
+    public static String getUserGuess(View view, InputValidation inputValidation) {
+        view.printGameInterface("숫자입력요청");
+        String input = readLine();
+        inputValidation.validateInputNumberFromUser(input);
+        return input;
+    }
+    /*재게임 의사를 묻고 진행시키는 메서드*/
+    public static String askNewGame(View view, InputValidation inputValidation) {
+        view.printGameInterface("정답맞춤");
+        String input = readLine();
+        inputValidation.validateRegameInputFromUser(input);
+        return input;
+
     }
 }
+
